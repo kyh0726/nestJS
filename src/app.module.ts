4aff2eb102dd4198de552e553fd3cce844e2aa76
younghyun753 <younghyun75398@gmail.com>
2025-05-08 21:54:17 +0900

    feat: main 로직 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { ChatModule } from './chat/chat.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [BoardsModule, ChatModule, UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(8080);
 }
 bootstrap();

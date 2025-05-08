7e2857794d946de5bdc4cbce332227dcb840710e
younghyun753 <younghyun75398@gmail.com>
2025-05-08 21:54:05 +0900

    feat: board 관련 crud 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardServece: BoardsService) {}
+    constructor(private boardService: BoardsService) {}
 
     @Get()
-    getAllBoards() {
-        return this.boardServece.getAllBoards();
+    getAllBoards(): Board[] {
+        return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title: string, 
+        @Body('description') description: string)
+        : Board {
+        return this.boardService.createBoard(title, description);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards;
     }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        };
+        this.boards.push(board); // boards 배열에 새로운 board 추가
+        return board;
+    }
+
 }
